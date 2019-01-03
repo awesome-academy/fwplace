561c79af925d13ebd59dc83889f3d70e98e372b8
Nguyễn Hồng Hải <nguyenhonghai1997bg@gmail.com>
2019-01-03 13:02:24 +0700

    show report2

---
+++ app/Repositories/ReportRepository.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use App\Models\Batch;
+use App\Models\Subject;
+use Illuminate\Support\Facades\DB;
+
+class ReportRepository extends EloquentRepository
+{
+    public function model()
+    {
+        return \App\Models\Report::class;
+    }
+
+    public function getReports($search)
+    {
+        $this->makeModel();
+
+        return $this->model->join('users', 'reports.id', 'users.id')
+                    ->where('name', 'Like', '%' . $search . '%')
+                    ->orderBy('name')
+                    ->select('reports.*', 'users.name', 'users.email', 'users.school')
+                    ->paginate(config('paginate'));
+    }
+
+    public function getReportsBySubjectID($subjectID, $userID)
+    {
+        $this->makeModel();
+
+        return $this->model
+            ->select('reports.*', 'reviews.id as review_id', DB::raw('COALESCE(reviews.content, \'\') as review'))
+            ->leftJoin('reviews', 'reports.id', 'reviews.report_id')
+            ->where('reports.user_id', $userID)
+            ->where('subject_id', $subjectID)
+            ->orderBy('subject_id', 'asc')
+            ->orderBy('day', 'asc')
+            ->get()->toArray();
+    }
+
+    public function getReportsBySubject($search)
+    {
+        $this->makeModel();
+
+        return $this->model->join('subjects', 'reports.subject_id', 'subjects.id')
+                    ->where('reports.subject_id', $search)
+                    ->orderBy('reports.id')
+                    ->select('reports.*', 'subjects.name')
+                    ->get();
+    }
+
+    public function getReportsGroupBySubject($userID)
+    {
+        $user = User::findOrFail($userID);
+        $batch = Batch::findOrFail($user->batch_id);
+        $subjects = $batch->subjects;
+        $result = [];
+        foreach ($subjects as $subject) {
+            $this->makeModel();
+            $data = $this->getReportsBySubjectID($subject->id, $userID);
+            $result = array_merge($result, [$data]);
+        }
+
+        return $result;
+    }
+}
