0746f8c9efd4e957e79159104662b2ea1b637b6f
2751997nam <nambcvt275@gmail.com>
2019-02-20 10:01:44 +0700

    fix diagram image

--- resources/views/test/workspace/diagram_detail.blade.php
+++ resources/views/test/workspace/diagram_detail.blade.php
@@ -7,10 +7,16 @@
 @section('content')
 <div class="m-content">
     <div class="row">
+        @if(isset($diagramDetail) && $diagramDetail->diagramContent)
+            {!! $diagramDetail->diagramContent !!}
+        @else
+            <div class="text-center">
+                <span class="text-danger">{{ __('This workspace don\'t have this kind of diagram') }}</span>
+            </div>
+        @endif
         <div class="col-lg-12 text-center">
             {!! Html::image($diagramDetail->DesignDiagram, null, ['usemap' => '#image-map']) !!}
         </div>
-        {!! $diagramDetail->content !!}
     </div>
 </div>
 @endsection
